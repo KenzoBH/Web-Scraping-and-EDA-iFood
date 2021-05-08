a552787c3450bf28d94dc62936f00a2949f58d22
Bruno Kenzo <brunokenzoh@hotmail.com>
2021-05-08 20:26:49 -0300

    Renamed the file and updates
    Added some comments and the n_scolls variable.

--- web-scraper-ifood.py
+++ web-scraper-ifood.py
@@ -1,38 +1,46 @@
 # ----------------------------------------------------
 #
-# This program is a Web Scraping proccess, to get data
-# from IFood website, for future analysis.
+# This program is a Web Scraping proccess to get data
+# from IFood website, for future analysis
+# (the comments in this code are in English).
+# - Bruno Kenzo, http://kenzobh.github.io.
 #
 # ----------------------------------------------------
 
-import pandas as pd
-from selenium.webdriver import Chrome, ChromeOptions
+import pandas as pd # Data manipulation and DataFrame creation
+from selenium.webdriver import Chrome, ChromeOptions # For Web Scraping
 import time
 
-browser = Chrome(executable_path = 'ADD_CHROMEDRIVE.EXE_PATH')
+browser = Chrome(executable_path = 'ADD-YOUR-chromedriver.exe-PATH')
 
-# Open the iFood page
+n_scrolls = 8 # Number of scrolls in the page, to get more data
+
+# Open the iFood page: it will search for restaurants in a 10km distance radius
 ifood_url = 'https://www.ifood.com.br/lista-restaurantes?f_distance=10'
 browser.get(ifood_url)
 
+# Clicks on "Usar minha localização" button
 # <button class="btn-address--full-size" aria-label="Usar minha localização">
 use_my_location_button = browser.find_element_by_xpath('//button[@class="btn-address--full-size"]')
 use_my_location_button.click()
 time.sleep(3)
 
+# Clicks on "Ver mais restaurantes e mercados" button "n_scrolls" times
 # <button type="button" role="button" class="btn btn--default btn--white btn--size-m btn--full-width restaurants-list__load-more" aria-label="Ver mais restaurantes e mercados" color="white" variant="restaurants-list__load-more" theme="default" target="" rel="">
-for _ in range(5): # Scrolls the page 5 times, to get more restaurants
+for _ in range(n_scrolls): # Scrolls the page 5 times, to get more restaurants
     browser.execute_script("window.scrollTo(0, document);")
     time.sleep(3)
     load_more_button = browser.find_element_by_xpath('//button[@class="btn btn--default btn--white btn--size-m btn--full-width restaurants-list__load-more"]')
     load_more_button.click()
     time.sleep(3)
 
+# Get the restauraurant name
 # <span class="restaurant-name">
 restaurants = [rest.text for rest in browser.find_elements_by_xpath('//span[@class="restaurant-name"]')]
 # print(restaurants)
 # print(len(restaurants))
 
+# Get the rate, the food type and the distance from me
 # <div class="restaurant-card__info">
 some_info = [info.text for info in browser.find_elements_by_xpath('//div[@class="restaurant-card__info"]')]
 # print(some_info) # Element example: '4.9\n•\nLanches\n•\n3,6 km'
@@ -45,6 +53,7 @@ for i in some_info: # The data comes dirty: '4.9\n•\nLanches\n•\n3,6 km', th
     types.append(i.split('\n•\n')[1])
     distances.append(float(i.split('\n•\n')[2][:-3].replace(',', '.')))
 
+# Get the time distance and the greight price from each restaurant
 # <div class="restaurant-card__footer">
 times_and_freight_prices = [info.text for info in browser.find_elements_by_xpath('//div[@class="restaurant-card__footer"]')]
 # print(times_and_freight_prices)
@@ -59,11 +68,11 @@ for i in times_and_freight_prices: # The data comes dirty here too
         freight_prices.append(float(i.split('\n•\n')[1][3:].replace(',', '.')))
 
 # Creates a Dataframe and saves in a csv file
-df = pd.DataFrame({'Restaurant': restaurants,
-                   'Rate': ratings,
-                   'Type': types,
-                   'Distance': distances,
-                   'Time': times,
-                   'Freight Price': freight_prices})
-df.to_csv('data.csv') # Clean data!
-# print(df)
+df = pd.DataFrame({'Restaurante': restaurants,
+                   'Nota': ratings,
+                   'Tipo de comida': types,
+                   'Distancia': distances,
+                   'Tempo': times,
+                   'Preco do Frete': freight_prices})
+df.to_csv('dados-ifood.csv') # Clean data!
+print(df)
